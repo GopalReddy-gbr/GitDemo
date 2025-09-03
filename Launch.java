6bede6cec592becda16bfd72d20f2f73a5aa76c3
GopalReddy-gbr <gopalreddygbr001@gmail.com>
2025-09-03 21:40:02 +0530

    commited after changes

--- Launch.java
+++ Launch.java
@@ -2,5 +2,15 @@ class Demo{
 	public static void main(String[] args){
 		System.out.println("Hello world!");
 		System.out.println("Changes are made");
+		add();
+	}
+
+
+	public static void add() {
+		int a = 10;
+		int b = 20;
+
+		int sum = a + b;
+		System.out.println(sum);
 	}
 }
